0b365d32e40211825b3766e637b18769dd1aa788
moderndaymoron <ragnaringir@gmail.com>
2016-02-03 13:18:16 +0000

    indent

--- Assignment2/Lexer.py
+++ Assignment2/Lexer.py
@@ -1,29 +1,34 @@
-from enum import enum
+from enum import Enum
+import sys
+import Token
 TokenCode = Enum('TokenCode', 'ID ASSIGN SEMICOL INT ADD SUB MULT LPAREN RPAREN PRINT END ERROR')
 class Lexer:
-	def __inti__(self):
-		pass
+    tokens = []
+    def __inti__(self):
+        pass
     def nextToken():
-    	while True:
-    		c = sys.stdin.read(1)
-    		if c == "=":
-    			token = Token("=", 2)
-    		elif c == ";":
-    			token = Token(";", 3)
-    		elif c == "+":
-    			pass
-    		elif c == "-":
-    			pass
-    		elif c == "*":
-    			pass
-    		elif c == "(":
-    			pass
-    		elif c == ")":
-				pass
-			elif c.isdigit():
-				pass
-			elif c.isalpha():
-				pass
-			else:
-				pass
-				#error
+        while True:
+    	    c = sys.stdin.read(1)
+    	    if c == "=":
+    	        token = Token("=", 2)
+    		    tokens.append(token)
+            elif c == ";":
+    	        token = Token(";", 3)
+    		    tokens.append(token)
+            elif c == "+":
+    		    pass
+            elif c == "-":
+                pass
+            elif c == "*":
+                pass
+            elif c == "(":
+                pass
+            elif c == ")":
+                pass
+            elif c.isdigit():
+                pass
+            elif c.isalpha():
+                pass
+            else:
+                pass
+                #error
